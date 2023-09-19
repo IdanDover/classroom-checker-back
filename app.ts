3a6ac0d093910d7149d52df46a10f968ee0ec609
Idan Dover <idan.dava@gmail.com>
2023-09-19 23:15:06 +0300

    it get files  now

--- app.ts
+++ app.ts
@@ -1,13 +1,23 @@
 import express, { Express, Request, Response } from "express";
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 import excel from "./excelParser";
 
 const app: Express = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
+
+app.post("/upload_files", upload.single("file"), uploadFiles);
+
+function uploadFiles(req: Request, res: Response) {
+  console.log(req.file);
+  res.json({ message: "Successfully uploaded files" });
+}
 
 export = app;
